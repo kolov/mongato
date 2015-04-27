61ed067a573cc2ef7b1325e46ce4f80cf1c7153a
Assen Kolov <assen.kolov@gmail.com>
2015-04-27 21:23:53 +0200

    simplified def

--- src/mongato/renderers.clj
+++
@@ -1,9 +0,0 @@
-(ns mongato.renderers)
-
-; Useful render functions
-
-(defn last4 [id]
-  "Renders only the end of an id, e.g.: ..NNNN"
-  (let [s (str id)]
-    (str ".." (apply str (drop (- (count s) 4) s)))
-    ))
